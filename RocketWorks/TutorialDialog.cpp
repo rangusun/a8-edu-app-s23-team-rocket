a478cdd84df1f65917062212a6ba982c78b16727
Anna <93408862+GalacticRadio@users.noreply.github.com>
2023-04-17 15:25:15 -0600

    Cleaned Up Tutorial Dialog Text & Fixed Tutorial Window Size

--- RocketWorks/TutorialDialog.cpp
+++ RocketWorks/TutorialDialog.cpp
@@ -24,25 +24,25 @@ QString TutorialDialog::getText(RocketComponent component)
         return "This is what gives color to your fireworks!\n"
                "- Stars are the most important component of a firework and the part you see in the night sky. They are small, flammable objects that get ignited and dispersed into the air by the blast charge when the firework shell is at a sufficient height.\n"
                "- Stars can be made of a variety of metal salts and other chemical compounds to control color, sound, and light effects.\n"
-               "- Different Materials For Star Color:\n"
-               "- Strontium Salts: Creates red fireworks\n"
-               "- Calcium Salts: Creates orange fireworks\n"
-               "- Sodium Salts: Creates yellow fireworks\n"
-               "- Barium Salts: Creates green fireworks\n"
-               "- Copper Salts: Create blue fireworks\n"
-               "- Strontium + Copper Compounds: Creates purple fireworks\n"
-               "- Aluminum: Creates white fireworks\n"
                "- Stars can be arranged in fun patterns inside the shell to create interesting firework shapes. These patterns are often created using cardboard, with stars glued along the outside.\n"
+               "- Different Star Materials and the Colors they create:\n"
+               "  - Red: Strontium Salts\n"
+               "  - Orange: Calcium Salts\n"
+               "  - Yellow: Sodium Salts\n"
+               "  - Green: Barium Salts\n"
+               "  - Blue: Copper Salts\n"
+               "  - Purple: Strontium + Copper Compounds\n"
+               "  - White: Aluminum and other metals\n"
                "- Star Components That Create Sounds:\n"
-               "- Bangs: Compacted, confined gun powder\n"
-               "- Crackling: Bismuth compounds divided in small granules\n"
-               "- Whistles: Aromatic organic compounds mixed with oxidiser\n";
+               "  - Bangs: Compacted, confined gun powder\n"
+               "  - Crackling: Bismuth compounds divided in small granules\n"
+               "  - Whistles: Aromatic organic compounds mixed with oxidiser\n";
     case liftCharge:
         return "This is what gives the size of the firework display!\n"
-               "- Blast charges are made out of black powder. They send firework stars off flying when they are ignited by the time delay fuse in a firework. The time delay fuse has to be made to the right length and burn time so that the firework does not ignite when it’s too close to the ground.";
+               "Blast charges are made out of black powder. They send firework stars off flying when they are ignited by the time delay fuse in a firework. The time delay fuse has to be made to the right length and burn time so that the blast charge does not explode while the firework is too close to the ground.";
     case blastCharge:
         return "This is what gives your fireworks height!\n"
-               "- Lift charges are placed in between the shell and the bottom of the launch tube of the firework which when ignited launches the firework upwards.\n"
+               "- Lift charges sit underneath the shell, on the bottom of the launch tube. When the lift charge is ignited by a fuse, it launches the firework upwards and ignites fuse, the time delay fuse.\n"
                "- Lift charges contain black powder, which is gun powder that is made with charcoal, sulfur, and potassium nitrate.\n"
                "- Lift charges would not work without a launch tube - they rely on pressure buildup inside a closed space to propel the firework upward.\n"
                "- The larger the lift charge is, the higher your firework will be launched!\n";
@@ -50,7 +50,7 @@ QString TutorialDialog::getText(RocketComponent component)
         return "- Fuse: Ignites the lift charge\n"
                "- Time Delay Fuse: Ignited by the lift charge, ignites the blast charge after a delay.\n"
                "- Launch Tube: The firework shell is placed into the launch tube before ignition. The launch tube ensures that the lift charge will create enough pressure to blast the shell into the air. The launch tube itself stays on the ground.\n"
-               "- Shell: The container for all the stars and the blast charge. Essentially just the firework itself.\n";
+               "- Shell: The container for all the stars and the blast charge. Essentially just the outer casing for the firework itself.\n";
     }
 
     return "";
