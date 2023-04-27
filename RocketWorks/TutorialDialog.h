46ae6cd857197db40b991e15e6109e50b61ee8c9
Carlos Gonzalez <77692383+Greenking1014@users.noreply.github.com>
2023-04-27 00:10:19 -0600

    Documented the .h files for:
    FireworkShapeEnum.h
    ShellPreviewWidget.h
    TutorialDialog.h
    WorldObject.h

--- RocketWorks/TutorialDialog.h
+++ RocketWorks/TutorialDialog.h
@@ -1,3 +1,12 @@
+/**
+ *  A8: An-Educational-App
+ *
+ *  @brief This class will hold all the Dialog for the Tutorial.
+ *
+ *
+ *  By
+ *  for CS 3505 in Spring 2023
+*/
 #ifndef TUTORIALDIALOG_H
 #define TUTORIALDIALOG_H
 
@@ -14,9 +23,26 @@ class TutorialDialog : public QDialog
     Q_OBJECT
 
 public:
+    /**
+     * @brief – Constructor for Tutorial Dialog.
+     *
+     *
+     */
     explicit TutorialDialog(QWidget *parent = nullptr);
+
+    /**
+     * @brief – Destructor for Tutorial Dialog.
+     *
+     *
+     */
     ~TutorialDialog();
 
+    /**
+     * @brief – Sets the ui to show component dialog.
+     *
+     *
+     * @param - RocketComponent component - The type of component the user is looking at.
+     */
     void showComponentDialog(RocketComponent component);
 
 private:
@@ -26,8 +52,26 @@ private:
     QString text;
     QString title;
 
+    /**
+     * @brief – Getter for component text information.
+     *
+     * @param - RocketComponent component - The type of component the user is looking at.
+     * @return QString
+     */
     QString getText(RocketComponent component);
+
+    /**
+     * @brief – Getter for component title information.
+     *
+     * @param - RocketComponent component - The type of component the user is looking at.
+     */
     QString getTitle(RocketComponent component);
+
+    /**
+     * @brief – Getter for component image information.
+     *
+     * @param - RocketComponent component - The type of component the user is looking at.
+     */
     QPixmap getImage(RocketComponent component);
 };
 
--- RocketWorks/WorldObject.h
+++ RocketWorks/WorldObject.h
@@ -1,3 +1,13 @@
+/**
+ *  A8: An-Educational-App
+ *
+ *  @brief This class holds information about a WorldObject object and
+ *  holds functions that can make these objects from pixel, cartesian, and Box2D coordinates.
+ *
+ *
+ *  By
+ *  for CS 3505 in Spring 2023
+*/
 #ifndef WORLDOBJECT_H
 #define WORLDOBJECT_H
 
@@ -20,14 +30,76 @@ public:
     const double box2DX, box2DY;
     const double width, height;
 
+    /**
+     * @brief – Setter for the screen width and height.
+     *
+     *
+     * @param double screenWidth – The screen width.
+     * @param double screenHeight – The screen height.
+     */
     static void setScreenWidthAndHeight(double screenWidth, double screenHeight);
+
+    /**
+     * @brief – Setter for the zoom value.
+     *
+     *
+     * @param double zoom - The zoom value.
+     */
     static void setZoom(double zoom);
 
+    /**
+     * @brief – Creates a WorldObject using Cartesian coordniates.
+     *
+     * @param string name - The name of the object being created.
+     * @param double x - X cartesian value.
+     * @param double y - Y cartesian value.
+     * @param double width - Object width.
+     * @param double height - Object height.
+     * @return WorldObject
+     */
     static WorldObject makeWorldObjectFromCartCoords(string name, double x, double y, double width, double height);
+
+    /**
+     * @brief – Creates a WorldObject using Pixel coordniates.
+     *
+     * @param string name - The name of the object being created.
+     * @param double x - X pixel value.
+     * @param double y - Y pixel value.
+     * @param double width - Object width.
+     * @param double height - Object height.
+     * @return WorldObject
+     */
     static WorldObject makeWorldObjectfromPixelCoords(string name, double x, double y, double width, double height);
+
+    /**
+     * @brief – Creates a WorldObject using Box2D coordniates.
+     *
+     * @param string name - The name of the object being created.
+     * @param double x - X Box2D value.
+     * @param double y - Y Box2D value.
+     * @param double width - Object width.
+     * @param double height - Object height.
+     * @return WorldObject
+     */
     static WorldObject makeWorldObjectFromBox2DCoords(string name, double x, double y, double width, double height);
 
 private:
+    /**
+     * @brief – Creates a WorldObject object.
+     *
+     * @param string name - The name of the object being created.
+     * @param double cartX - X cartesian value.
+     * @param double cartY - Y cartesian value.
+     * @param double pixelX - X pixel value.
+     * @param double pixelY - Y pixel value.
+     * @param double drawX - Drawing x value.
+     * @param double drawY - Drawing y value.
+     * @param double box2DX - X Box2D value.
+     * @param double box2DY - Y Box2D value.
+     * @param double width - Object width.
+     * @param double height - Object height.
+     * @return WorldObject
+     */
     explicit WorldObject(string name, double cartX, double cartY, double pixelX, double pixelY, double drawX,
                          double drawY, double box2DX, double box2DY, double width, double height);
 };
